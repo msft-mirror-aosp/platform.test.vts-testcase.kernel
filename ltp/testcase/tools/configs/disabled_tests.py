e53d45218f52e3f173cf6212fc4a9b5d35cb1850
Edward Liaw <edliaw@google.com>
2022-09-15 15:18:06 +0000

    ltp: remove cve dupes
    The cve tests are duplicates of certain tests that are aliased by the
    security issue id.  Remove the duplicated tests and leave the cve
    aliases enabled.
    e.g. pty03 is aliased as cve.cve-2020-14416.
    
    Bug: 191224822
    Test: atest -a vts_ltp_test_x86
    Test: atest vts_ltp_test_x86_64
    Signed-off-by: Edward Liaw <edliaw@google.com>
    Change-Id: Ibcfa236f59fa1bdbc60bb99f7795c0bd24c827ce

--- ltp/testcase/tools/configs/disabled_tests.py
+++ ltp/testcase/tools/configs/disabled_tests.py
@@ -38,8 +38,6 @@ DISABLED_TESTS = {
     'kernel_misc.zram01_64bit',  # b/191226875
     'kernel_misc.zram02_32bit',  # b/191227531
     'kernel_misc.zram02_64bit',  # b/191227531
-    'pty.pty03_32bit',  # b/191224822
-    'pty.pty03_64bit',  # b/191224822
     'pty.pty05_32bit',  # b/191224341
     'pty.pty05_64bit',  # b/191224341
     'sched.sched_getattr01_32bit', # b/200686092
