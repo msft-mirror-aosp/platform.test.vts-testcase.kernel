e63e7f6bb95dbfa7a8bf3c1169e4158e651e0fbe
Wake Liu <wakel@google.com>
2025-11-25 15:19:21 +0800

    ltp: mark syscalls.set_thread_area02_64bit as non-mandatory.
    The test reports that it does not support the x86_64 architecture
    and exits with a TCONF error. The test runner interprets this skipped
    test as a failure.
    
    Flag: TEST_ONLY
    Test: N/A
    Bug: 463541890
    Change-Id: I3f1cd4e0aa9a67122f2da334729e6fbe20372104
    Signed-off-by: Wake Liu <wakel@google.com>

--- ltp/testcase/tools/configs/stable_tests.py
+++ ltp/testcase/tools/configs/stable_tests.py
@@ -2578,7 +2578,7 @@ STABLE_TESTS = {
     'syscalls.set_robust_list01_64bit': True,
     'syscalls.set_thread_area01_32bit': False,
     'syscalls.set_thread_area02_32bit': True,
-    'syscalls.set_thread_area02_64bit': True,
+    'syscalls.set_thread_area02_64bit': False,  # b/463541890
     'syscalls.set_tid_address01_32bit': True,
     'syscalls.set_tid_address01_64bit': True,
     'syscalls.setdomainname01_32bit': True,
