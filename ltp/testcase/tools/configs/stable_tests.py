63f76f152ec0c43cc847612b6fef95edbefd9a6a
Darren Chang <chihsheng@google.com>
2025-12-24 07:52:54 +0000

    ltp: mark syscalls.mount_setattr02_64bit as non-mandatory
    Test case "syscalls.mount_setattr02_64bit" didn't support on specific arch,
    so temporarily mark the specific test case as non-mandatory.
    
    Test: USE_RBE=false RBE_enable=false atest -s 127.0.0.1:41563 vts_ltp_test_x86_64
    Bug: 471350031
    Change-Id: I981c3a41e1d3a578ac3acfd2a961259e8fc6d645
    Signed-off-by: Darren Chang <chihsheng@google.com>

--- ltp/testcase/tools/configs/stable_tests.py
+++ ltp/testcase/tools/configs/stable_tests.py
@@ -1986,7 +1986,7 @@ STABLE_TESTS = {
     'syscalls.mount_setattr01_32bit': False,
     'syscalls.mount_setattr01_64bit': False,
     'syscalls.mount_setattr02_32bit': True,
-    'syscalls.mount_setattr02_64bit': True,
+    'syscalls.mount_setattr02_64bit': False,  # b/471350031
     'syscalls.move_mount03_32bit': False,  # b/436690544
     'syscalls.move_mount03_64bit': False,  # b/436690544
     'syscalls.mprotect01_32bit': True,
