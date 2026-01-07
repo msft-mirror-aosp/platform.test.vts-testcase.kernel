a5543c9570a8b6a0aa15e0ffba9bbb864c44dc83
Darren Chang <chihsheng@google.com>
2026-01-07 02:06:36 +0000

    ltp: mark cve.cve-2025-38236_32bit as non-mandatory due to the  unsuitable kernel config
    Test case "cve.cve-2025-38236_32bit" can't work with unsuitable kernel config:
    "CONFIG_AF_UNIX_OOB Undefined", so temporarily mark the specific test case as non-mandatory.
    
    Test: local
    Bug: 471342776
    Change-Id: I467518dc11f226dada7c31ef99f780c1c8b0f610
    Signed-off-by: Darren Chang <chihsheng@google.com>

--- ltp/testcase/tools/configs/stable_tests.py
+++ ltp/testcase/tools/configs/stable_tests.py
@@ -313,7 +313,7 @@ STABLE_TESTS = {
     'cve.cve-2022-4378_64bit': False,
     'cve.cve-2023-1829_32bit': False,
     'cve.cve-2023-1829_64bit': False,
-    'cve.cve-2025-38236_32bit': True,
+    'cve.cve-2025-38236_32bit': False,  # b/471342776
     'cve.cve-2025-38236_64bit': False,  # b/471342776
     'dio.dio01_32bit': False,
     'dio.dio01_64bit': False,
